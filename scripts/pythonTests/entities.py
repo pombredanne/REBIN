3eb32e2a266b039706f5051aa48cd02c666f3dc6
Anthony Nystrom <nystrom.anthony@gmail.com>
2013-02-26 22:42:20 -0800

    Add - Py Script
    Entities script. NLTK chunked of input text.

---
+++ scripts/pythonTests/entities.py
@@ -0,0 +1,11 @@
+#! /usr/local/bin/python
+
+import sys
+import nltk
+
+input = sys.argv[1]
+
+tokens = nltk.word_tokenize(input)
+tagged = nltk.pos_tag(tokens)
+entities = nltk.chunk.ne_chunk(tagged)
+print entities
